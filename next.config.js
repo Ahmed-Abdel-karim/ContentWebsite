aac837d5ba4f4e921f455715e3c3b9760dcf2637
ahmed.abdelkarim <ahmed.abdelkarim@nozol.com>
2021-02-27 15:00:35 +0200

    provide pwa support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  // trailingSlash: true,
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
   images: {
-    domains: ['swagger.city-edge-developments.com']
-  }
-}
+    domains: ["swagger.city-edge-developments.com"],
+  },
+});
